717d4b8af58a5e9f42168b9a807f69f6ed6b7480
Felipe Dumont <felipedumontt@hotmail.com>
2022-06-26 15:59:55 -0300

    Feat: Change server command reply to test

--- index.js
+++ index.js
@@ -1,38 +1,38 @@
 // Require the necessary discord.js classes
+const { format } = require('date-fns');
 const { Client, Intents } = require('discord.js');
 require('dotenv').config();
 
 // Create a new client instance
 const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
 
-async function main() {
-  // When the client is ready, run this code (only once)
-  client.once('ready', () => {
-    console.log('Ready!');
-  });
+// When the client is ready, run this code (only once)
+client.once('ready', () => {
+  console.log('Ready!');
+});
 
-  client.on('interactionCreate', async (interaction) => {
-    if (!interaction.isCommand()) return;
+client.on('interactionCreate', async (interaction) => {
+  if (!interaction.isCommand()) return;
 
-    const { commandName } = interaction;
+  const { commandName } = interaction;
 
-    if (commandName === 'ping') {
-      await interaction.reply('Pong!');
-    } else if (commandName === 'server') {
-      await interaction.reply('Server info testeeeee.');
-    } else if (commandName === 'user') {
-      await interaction.reply('User info.');
-    }
-  });
-
-  // Login to Discord with your client's token
-  await client.login(process.env.BOT_TOKEN);
-
-  await cleanUp();
-
-  async function cleanUp() {
-    client.destroy();
+  if (commandName === 'ping') {
+    await interaction.reply('Pong!');
+  } else if (commandName === 'server') {
+    await interaction.reply(
+      `Nome do servidor: ${interaction.guild.name}
+      Total de mrmbros: ${interaction.guild.memberCount}
+      Data de criação: ${format(
+        new Date(interaction.guild.createdAt),
+        'dd/MM/yyyy',
+        { locale: 'pt-BR' }
+      )}
+      `
+    );
+  } else if (commandName === 'user') {
+    await interaction.reply('User info.');
   }
-}
+});
 
-main();
+// Login to Discord with your client's token
+client.login(process.env.BOT_TOKEN);
